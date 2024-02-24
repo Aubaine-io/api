410d3cd79fc61e2e78b1766d24b1a69c13a12ca8
Aymen EZZAYER <aymen.ezzayer@epita.fr>
2024-02-24 22:13:50 +0100

    ✨ `app.database.attributes` now reads from `.env` file

--- app/database/attributes.py
+++ app/database/attributes.py
@@ -1,8 +1,12 @@
+from dotenv import load_dotenv
 import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+# Load `.env` file
+load_dotenv()
 
 engine = create_engine(os.getenv("DATABASE_URL"))
 
